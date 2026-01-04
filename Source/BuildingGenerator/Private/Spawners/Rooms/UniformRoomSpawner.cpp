c6cc662270fea113234a7165a378c9465922e30c
DreadDev1 <kameen.avery@gmail.com>
2026-01-04 00:47:59 -0500

    Create AUniformRoomSpawner class

---
+++ Source/BuildingGenerator/Private/Spawners/Rooms/UniformRoomSpawner.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spawners/Rooms/UniformRoomSpawner.h"
+
+
+// Sets default values
+AUniformRoomSpawner::AUniformRoomSpawner()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AUniformRoomSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUniformRoomSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/BuildingGenerator/Public/Spawners/Rooms/UniformRoomSpawner.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Spawners/Rooms/RoomSpawner.h"
+#include "UniformRoomSpawner.generated.h"
+
+UCLASS()
+class BUILDINGGENERATOR_API AUniformRoomSpawner : public ARoomSpawner
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AUniformRoomSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
